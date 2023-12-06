a415405d57093fa85570b4d41ab17f39963339d2
George V. <georgev22.github@gmail.com>
2023-12-06 22:16:37 +0200

    feat: Added JavaDocs to inventory classes and new methods to VoidInventory

--- src/main/java/com/georgev22/voidchest/api/inventory/InventoryManager.java
+++ src/main/java/com/georgev22/voidchest/api/inventory/InventoryManager.java
@@ -5,17 +5,60 @@ import org.jetbrains.annotations.NotNull;
 
 import java.util.UUID;
 
+/**
+ * The {@code InventoryManager} interface provides methods for managing void chest inventories.
+ * Implementations of this interface are responsible
+ * for creating and accessing different types of void chest inventories.
+ *
+ * <p>Void chest inventories are associated with a specific {@link IVoidStorage}, and each inventory type is represented
+ * by the {@link InventoryType} enum.
+ */
 public interface InventoryManager {
 
-    @NotNull VoidInventory inventory(IVoidStorage voidStorage, InventoryType type);
+    /**
+     * Creates a void chest inventory associated with the specified {@link IVoidStorage} and of the specified type.
+     *
+     * @param voidStorage The void storage associated with the inventory.
+     * @param type        The type of inventory to create.
+     * @return A non-null instance of the created void chest inventory.
+     * @throws NullPointerException if {@code voidStorage} is {@code null}.
+     */
+    @NotNull
+    VoidInventory inventory(IVoidStorage voidStorage, InventoryType type);
 
-    @NotNull VoidInventory inventory(UUID voidStorageUUID, InventoryType type);
+    /**
+     * Retrieves a void chest inventory associated with the specified void storage UUID and of the specified type.
+     *
+     * @param voidStorageUUID The UUID of the void storage associated with the inventory.
+     * @param type            The type of inventory to retrieve.
+     * @return A non-null instance of the retrieved void chest inventory.
+     * @throws NullPointerException if {@code voidStorageUUID} is {@code null}.
+     */
+    @NotNull
+    VoidInventory inventory(UUID voidStorageUUID, InventoryType type);
 
+    /**
+     * An enumeration representing different types of void chest inventories.
+     */
     enum InventoryType {
+        /**
+         * Represents the storage inventory type for storing items in a void chest.
+         */
         STORAGE,
+
+        /**
+         * Represents the whitelist inventory type for managing whitelist entries in a void chest.
+         */
         WHITELIST,
+
+        /**
+         * Represents the blacklist inventory type for managing blacklist entries in a void chest.
+         */
         BLACKLIST,
+
+        /**
+         * Represents the menu inventory type for displaying a menu in a void chest.
+         */
         MENU
     }
-
 }
--- src/main/java/com/georgev22/voidchest/api/inventory/VoidInventory.java
+++ src/main/java/com/georgev22/voidchest/api/inventory/VoidInventory.java
@@ -9,16 +9,38 @@ import org.jetbrains.annotations.NotNull;
 import org.jetbrains.annotations.Nullable;
 
 import java.math.BigInteger;
-import java.util.Arrays;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
+import java.util.function.Consumer;
 import java.util.stream.Collectors;
 
+/**
+ * The {@code VoidInventory} interface represents a void chest inventory,
+ * extending the Bukkit {@link Inventory} interface.
+ * It provides methods for adding, retrieving, and iterating over items within the void chest.
+ *
+ * <p>Items within the void chest are represented by {@link VoidInventoryItemStack}, which encapsulates both
+ * a {@link SerializableItemStack} for data serialization and an {@link ItemStack} for the visible representation
+ * of the item in the inventory.
+ *
+ * <p>This interface extends the Bukkit {@link Inventory} interface and adds additional functionality for handling
+ * void chest-specific operations.
+ */
 public interface VoidInventory extends Inventory {
 
+    /**
+     * Adds an array of {@link SerializableItemStack}s to the void chest.
+     *
+     * @param items The serializable item stacks to add.
+     * @return An {@link ObjectMap} containing the slot indices and corresponding serializable item stacks.
+     */
     ObjectMap<Integer, SerializableItemStack> addItems(SerializableItemStack... items);
 
+    /**
+     * Adds an array of {@link ItemStack}s to the void chest, converting them to {@link SerializableItemStack}s.
+     *
+     * @param items The item stacks to add.
+     * @return An {@link ObjectMap} containing the slot indices and corresponding item stacks.
+     */
     default ObjectMap<Integer, ItemStack> addItems(ItemStack... items) {
         return this.addItems(
                         Arrays.stream(items).map(
@@ -30,6 +52,12 @@ public interface VoidInventory extends Inventory {
                 ));
     }
 
+    /**
+     * Overrides the default Bukkit {@link Inventory#addItem(ItemStack...)} method to use void chest-specific behavior.
+     *
+     * @param items The item stacks to add.
+     * @return A {@link HashMap} containing the slot indices and corresponding item stacks.
+     */
     @Override
     @NotNull
     default HashMap<Integer, ItemStack> addItem(ItemStack... items) {
@@ -39,14 +67,29 @@ public interface VoidInventory extends Inventory {
         ));
     }
 
-
+    /**
+     * Retrieves a list of {@link VoidInventoryItemStack}s representing all items in the void chest.
+     *
+     * @return The list of void chest item stacks.
+     */
     List<VoidInventoryItemStack> getItems();
 
+    /**
+     * Sets the items in the void chest to the specified list of {@link VoidInventoryItemStack}s.
+     *
+     * @param items The list of void chest item stacks to set.
+     */
     default void setItems(List<VoidInventoryItemStack> items) {
         this.getItems().clear();
         this.getItems().addAll(items);
     }
 
+    /**
+     * Retrieves the {@link VoidInventoryItemStack} at the specified slot in the void chest.
+     *
+     * @param slot The slot index.
+     * @return The void chest item stack at the specified slot, or {@code null} if the slot is empty.
+     */
     default @Nullable VoidInventoryItemStack getVoidInventoryItem(int slot) {
         if (this.getItems().size() <= slot) {
             return null;
@@ -54,4 +97,46 @@ public interface VoidInventory extends Inventory {
         return getItems().get(slot);
     }
 
+    /**
+     * Iterates over the original {@link ItemStack}s of all items in the void chest.
+     *
+     * @param action The action to be performed on each original item stack.
+     */
+    @Override
+    default void forEach(Consumer<? super ItemStack> action) {
+        this.forEachOriginalItemStacks(action);
+    }
+
+    /**
+     * Iterates over the original item stacks of all items in the void chest using a specified action.
+     *
+     * @param action The action to be performed on each original item stack.
+     */
+    default void forEachOriginalItemStacks(Consumer<? super ItemStack> action) {
+        this.getItems().stream()
+                .filter(Objects::nonNull)
+                .map(VoidInventoryItemStack::getOriginalItemStack)
+                .forEach(action);
+    }
+
+    /**
+     * Iterates over the visible item stacks of all items in the void chest using a specified action.
+     *
+     * @param action The action to be performed on each visible item stack.
+     */
+    default void forEachVisibleItemStacks(Consumer<? super ItemStack> action) {
+        this.getItems().stream()
+                .filter(Objects::nonNull)
+                .map(VoidInventoryItemStack::getVisibleItemStack)
+                .forEach(action);
+    }
+
+    /**
+     * Iterates over all {@link VoidInventoryItemStack}s in the void chest using a specified action.
+     *
+     * @param action The action to be performed on each void chest item stack.
+     */
+    default void forEachVoidItemStack(Consumer<? super VoidInventoryItemStack> action) {
+        this.getItems().forEach(action);
+    }
 }
