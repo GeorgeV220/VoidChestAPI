62336a1c458beed9842fd466fe57ab46d5dbfdc8
George V. <georgev22.github@gmail.com>
2023-12-20 12:22:16 +0200

    feat(IEconomyManager): Added IEconomyManager.isHooked

--- src/main/java/com/georgev22/voidchest/api/economy/IEconomyManager.java
+++ src/main/java/com/georgev22/voidchest/api/economy/IEconomyManager.java
@@ -2,6 +2,7 @@ package com.georgev22.voidchest.api.economy;
 
 /**
  * The IEconomyManager interface provides methods for managing an economy system.
+ * Note: This manager hooks into an Economy Plugin to deposit and withdraw money but also get player balances.
  */
 public interface IEconomyManager {
 
@@ -9,6 +10,13 @@ public interface IEconomyManager {
      * Hooks the economy manager.
      */
     void hook();
+
+    /**
+     * Checks if the economy system is hooked.
+     *
+     * @return True if the economy system is hooked, false otherwise.
+     */
+    boolean isHooked();
 
     /**
      * Retrieves the economy instance.
