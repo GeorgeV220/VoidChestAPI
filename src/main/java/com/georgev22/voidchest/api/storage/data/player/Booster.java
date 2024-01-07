468c2d0c1d2b8bb807e5f5cbe0e2076f4b3710ac
George V. <georgev22.github@gmail.com>
2024-01-07 23:07:03 +0200

    feat(Booster): add plugin support for booster methods (2.4.0)
    - Add Plugin parameter to booster(), boosterTimeLeft(), and boostTime() methods
    - Add corresponding overloaded methods for each plugin parameter
    - Import org.bukkit.plugin.Plugin class

--- src/main/java/com/georgev22/voidchest/api/storage/data/player/Booster.java
+++ src/main/java/com/georgev22/voidchest/api/storage/data/player/Booster.java
@@ -1,6 +1,7 @@
 package com.georgev22.voidchest.api.storage.data.player;
 
 import com.georgev22.library.utilities.Entity;
+import org.bukkit.plugin.Plugin;
 
 /**
  * The Booster interface extends the Entity interface and provides methods for managing a booster.
@@ -15,11 +16,27 @@ public interface Booster extends Entity {
     double booster();
 
     /**
+     * Retrieves the booster value.
+     *
+     * @param plugin The plugin that added the booster.
+     * @return The booster value as a double.
+     */
+    double booster(Plugin plugin);
+
+    /**
      * Sets the booster value.
      *
      * @param booster The booster value to set as a double.
      */
     void booster(double booster);
+
+    /**
+     * Sets the booster value.
+     *
+     * @param booster The booster value to set as a double.
+     * @param plugin  The plugin that set the booster value.
+     */
+    void booster(double booster, Plugin plugin);
 
     /**
      * Retrieves the formatted time left for the booster.
@@ -29,6 +46,14 @@ public interface Booster extends Entity {
     String boosterTimeLeft();
 
     /**
+     * Retrieves the formatted time left for the booster.
+     *
+     * @param plugin The plugin that added the booster.
+     * @return The formatted time left as a String.
+     */
+    String boosterTimeLeft(Plugin plugin);
+
+    /**
      * Retrieves the boost time in milliseconds.
      *
      * @return The boost time in milliseconds as a long.
@@ -36,10 +61,26 @@ public interface Booster extends Entity {
     long boostTime();
 
     /**
+     * Retrieves the boost time in milliseconds.
+     *
+     * @param plugin The plugin that added the booster.
+     * @return The boost time in milliseconds as a long.
+     */
+    long boostTime(Plugin plugin);
+
+    /**
      * Sets the boost time in milliseconds.
      *
      * @param boostTime The boost time to set in milliseconds as a long.
      */
     void boostTime(long boostTime);
+
+    /**
+     * Sets the boost time in milliseconds.
+     *
+     * @param plugin    The plugin that adds the booster.
+     * @param boostTime The boost time to set in milliseconds as a long.
+     */
+    void boostTime(long boostTime, Plugin plugin);
 
 }
