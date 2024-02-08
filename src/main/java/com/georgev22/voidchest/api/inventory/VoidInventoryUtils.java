7ffeecfcd0c5e216fbd962d6d768ac74e5278fce
George V. <georgev22.github@gmail.com>
2024-02-08 18:19:54 +0200

    feat(inventory/API): Add new createInventory methods

--- src/main/java/com/georgev22/voidchest/api/inventory/VoidInventoryUtils.java
+++ src/main/java/com/georgev22/voidchest/api/inventory/VoidInventoryUtils.java
@@ -1,6 +1,7 @@
 package com.georgev22.voidchest.api.inventory;
 
 import com.georgev22.voidchest.api.inventory.holder.VoidInventoryHolder;
+import com.georgev22.voidchest.api.storage.data.IVoidStorage;
 import org.bukkit.entity.Player;
 import org.bukkit.inventory.Inventory;
 import org.jetbrains.annotations.NotNull;
@@ -41,6 +42,93 @@ public interface VoidInventoryUtils {
             @NotNull String inventoryName,
             int inventorySize,
             @NotNull List<VoidInventoryItemStack> inventoryItems
+    );
+
+    /**
+     * Creates a new VoidInventory.
+     *
+     * @param owner                   The owner of the inventory
+     * @param inventoryName           The name of the inventory
+     * @param inventorySize           The size of the inventory
+     * @param inventoryItems          The items to add to the inventory
+     * @param infiniteInventory       if the inventory is infinite
+     * @param infiniteVisibleItemLore the lore of the infinite visible items
+     * @return The created VoidInventory
+     */
+    @NotNull VoidInventory createInventory(
+            @Nullable VoidInventoryHolder owner,
+            @NotNull String inventoryName,
+            int inventorySize,
+            @NotNull List<VoidInventoryItemStack> inventoryItems,
+            boolean infiniteInventory,
+            @Nullable List<String> infiniteVisibleItemLore
+    );
+
+    /**
+     * Creates a new VoidInventory for the given VoidStorage.
+     * <p>
+     * VoidStorage parameter is required
+     * to be able to create an inventory that returns {@link VoidInventory#getLocation()}
+     * and also update the items stored stat for the VoidStorage.
+     *
+     * @param owner         The owner of the inventory
+     * @param inventoryName The name of the inventory
+     * @param inventorySize The size of the inventory
+     * @return The created VoidInventory
+     */
+    @NotNull VoidInventory createInventory(
+            @NotNull IVoidStorage voidStorage,
+            @Nullable VoidInventoryHolder owner,
+            @NotNull String inventoryName,
+            int inventorySize
+    );
+
+    /**
+     * Creates a new VoidInventory for the given VoidStorage.
+     * <p>
+     * VoidStorage parameter is required
+     * to be able to create an inventory that returns {@link VoidInventory#getLocation()}
+     * and also update the items stored stat for the VoidStorage.
+     *
+     * @param voidStorage    The {@link IVoidStorage} of the inventory
+     * @param owner          The owner of the inventory
+     * @param inventoryName  The name of the inventory
+     * @param inventorySize  The size of the inventory
+     * @param inventoryItems The items to add to the inventory
+     * @return The created VoidInventory
+     */
+    @NotNull VoidInventory createInventory(
+            @NotNull IVoidStorage voidStorage,
+            @Nullable VoidInventoryHolder owner,
+            @NotNull String inventoryName,
+            int inventorySize,
+            @NotNull List<VoidInventoryItemStack> inventoryItems
+    );
+
+    /**
+     * Creates a new VoidInventory for the given VoidStorage.
+     * <p>
+     * VoidStorage parameter is required
+     * to be able to create an inventory that returns {@link VoidInventory#getLocation()}
+     * and also update the items stored stat for the VoidStorage.
+     *
+     * @param voidStorage             The {@link IVoidStorage} of the inventory
+     * @param owner                   The owner of the inventory
+     * @param inventoryName           The name of the inventory
+     * @param inventorySize           The size of the inventory
+     * @param inventoryItems          The items to add to the inventory
+     * @param infiniteInventory       if the inventory is infinite
+     * @param infiniteVisibleItemLore the lore of the infinite visible items
+     * @return The created VoidInventory
+     */
+    @NotNull VoidInventory createInventory(
+            @NotNull IVoidStorage voidStorage,
+            @Nullable VoidInventoryHolder owner,
+            @NotNull String inventoryName,
+            int inventorySize,
+            @NotNull List<VoidInventoryItemStack> inventoryItems,
+            boolean infiniteInventory,
+            @Nullable List<String> infiniteVisibleItemLore
     );
 
     /**
