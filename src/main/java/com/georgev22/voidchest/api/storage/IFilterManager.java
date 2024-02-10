eefd5e9b64e2917faefd041508e5d948777da363
George V. <georgev22.github@gmail.com>
2024-02-10 17:18:25 +0200

    feat(storage/IFilterManager): add methods to check if item is allowed for collection
    - Add isItemAllowed methods to IFilterManager interface
    - Add VoidInventoryItemStack and Nullable annotations to parameters
    - Update javadoc comments for clarity and consistency

--- src/main/java/com/georgev22/voidchest/api/storage/IFilterManager.java
+++ src/main/java/com/georgev22/voidchest/api/storage/IFilterManager.java
@@ -1,7 +1,9 @@
 package com.georgev22.voidchest.api.storage;
 
+import com.georgev22.voidchest.api.inventory.VoidInventoryItemStack;
 import com.georgev22.voidchest.api.storage.data.IVoidStorage;
 import com.georgev22.voidchest.api.storage.data.voidstorage.Filter;
+import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
 
@@ -34,5 +36,28 @@ public interface IFilterManager {
      * @return The item filters of the VoidStorage.
      */
     List<Filter> getFilters(IVoidStorage voidChest);
+
+
+    /**
+     * Checks if the specified item is allowed for collection from the specified VoidChest.
+     * <p>
+     * Note: If voidStorage is null, this method will check on the global filters only.
+     *
+     * @param voidStorageName The VoidChest to check if the item is allowed.
+     * @param item            The item to check if it is allowed.
+     * @return True if the item is allowed, false otherwise.
+     */
+    boolean isItemAllowed(@Nullable String voidStorageName, VoidInventoryItemStack item);
+
+    /**
+     * Checks if the specified item is allowed for collection from the specified VoidChest.
+     * <p>
+     * Note: If voidStorage is null, this method will check on the global filters only.
+     *
+     * @param voidStorage The VoidChest to check if the item is allowed.
+     * @param item        The item to check if it is allowed.
+     * @return True if the item is allowed, false otherwise.
+     */
+    boolean isItemAllowed(@Nullable IVoidStorage voidStorage, VoidInventoryItemStack item);
 
 }
